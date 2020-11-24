f8a07634bd3caa17242072f03dafcefd0ea0a639
PhucNghia <ngocnghiaphuc@gmail.com>
2020-11-24 16:01:47 +0700

    create schedule

--- index.js
+++ index.js
@@ -1,60 +1,68 @@
 const bodyParser= require('body-parser')
 const https = require('https');
+const http = require('http');
 const fs = require('fs');
 const options = {
   key: fs.readFileSync('certificates/localhost-key.pem'),
   cert: fs.readFileSync('certificates/localhost.pem')
 };
-
+const request = require('request');
 var schedule = require('node-schedule');
 const restify = require('restify');
 const botbuilder = require('botbuilder');
 
-var cronExpress = '*/2 * * * * * *';
-var rule = new schedule.RecurrenceRule();
-rule.minute = 1;
-
-// schedule.scheduleJob(cronExpress, function(fireDate){
-//   console.log('running job!');
-//   console.log(fireDate);
+// Bot-inspector Create bot adapter, which defines how the bot sends and receives messages.
+// var adapter = new botbuilder.BotFrameworkAdapter({
+//   appId: "d2df0fd8-3ff3-4ec0-99ae-14adfb685866",
+//   appPassword: "-E5UJDO~S~z5hXj3U2dz5m9gT3lcwmp_3J"
 // });
 
-// Create bot adapter, which defines how the bot sends and receives messages.
+// Bot-mini
 var adapter = new botbuilder.BotFrameworkAdapter({
-  appId: "d2df0fd8-3ff3-4ec0-99ae-14adfb685866",
-  appPassword: "-E5UJDO~S~z5hXj3U2dz5m9gT3lcwmp_3J"
+  appId: "fd6f34a7-57dd-4030-87ca-ec58d3373a1f",
+  appPassword: "6Qehv.pwT.WXHf.PSvTYiD07hqI8D~I~58"
 });
 
 // Create HTTPs server.
-let server = restify.createServer(options, (req, res) => {
-  res.writeHead(200);
-  res.end('hello world\n');
-});
+// let server = restify.createServer(options, (req, res) => {
+//   res.writeHead(200);
+//   res.end('hello world\n');
+// });
+
+// Create HTTP server.
+let server = restify.createServer();
+
 server.listen(process.env.port || process.env.PORT || 7070, function () {
   console.log(`\n${server.name} listening to ${server.url}`);
   console.log(`\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator`);
 });
-var request = null;
-var response = null;
+var requestInfo = null;
+var responseInfo = null;
 
 // Listen for incoming requests at /api/messages.
 server.post('/api/messages', (req, res) => {
   // Use the adapter to process the incoming web request into a TurnContext object.
   console.log("req: " + req);
   console.log("res: " + res);
-  request = req;
-  response = res;
+  requestInfo = req;
+  responseInfo = res;
   adapter.processActivity(req, res, async (turnContext) => {
     let activity = turnContext.activity;
     console.log("activity type: " + activity.type);
     switch(activity.type) {
       case "conversationUpdate":
-        await turnContext.sendActivity("Welcome!");
-        await turnContext.sendActivities([
-          { type: 'typing' },
-          { type: 'delay', value: 4000 },
-          { type: 'message', text: 'Hello... How are you?' }
-       ]);
+        try {
+          await turnContext.sendActivity("Welcome!");
+          await turnContext.sendActivities([
+            // { type: 'typing' },
+            // { type: 'delay', value: 4000 },
+            { type: 'message', text: 'Hello... How are you?' }
+         ]);
+        } catch (error) {
+          console.log("Removed this Bot");
+          requestInfo = responseInfo = null;
+        }
+        
         break;
       case "message":
         // Get the user's text
@@ -80,12 +88,37 @@ server.get('/', (req, res) => {
   res.send("Hello");
 });
 
+// var cronExpress = '*/15 * * * * * *';  // 15s chạy 1 lần
+// var cronExpress = '0 5 8 * * THU';     // 8h30p thứ năm hàng tuần
+// var cronExpressMorning = '0 0 09 * * THU';
+// var cronExpressAfternoon = '0 30 15 * * THU';
+
+var cronExpressMorning = '*/30 * * * * * *';
+
+schedule.scheduleJob(cronExpressMorning, function(fireDate) {
+  let content = "hello 1";
+  processJob(content);
+  console.log(fireDate);
+});
+
+// schedule.scheduleJob(cronExpressAfternoon, function(fireDate) {
+//   let content = "hello 2";
+//   processJob(content);
+// });
+
+var processJob = function(content) {
+  if(requestInfo == null || responseInfo == null) {
+    return;
+  }
+  request.post('http://localhost:7070/api/notification', {form: {content: content}})
+}
+
 server.post('/api/notification', (req, res) => {
   let content = req.body.content;
   console.log("Content: " + content);
-  adapter.processActivity(request, response, async (turnContext) => {
+  adapter.processActivity(requestInfo, responseInfo, async (turnContext) => {
     await turnContext.sendActivity(content);
   });
   
-  res.send("send");
+  res.send("sent");
 });
